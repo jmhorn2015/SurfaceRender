e7c4e5d86331f3ac9cffdacab0fe65777207b948
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-02-26 15:23:08 -0600

    Update AddObject.js

--- js/AddObject.js
+++ js/AddObject.js
@@ -2,7 +2,8 @@ function AddObject(name2){
 	console.log("run");
 	var loader = new THREE.OBJLoader();
 	loader.load(name, function ( object ) {
-		scene.add( object );},
+		scene.add( object );
+		renderer.render( scene, camera );},
 	function ( xhr ) {
 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	},
