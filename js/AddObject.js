70950219f0f7764b3bf92a3d54068b018d7a204e
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-02 19:23:48 -0600

    test

--- js/AddObject.js
+++ js/AddObject.js
@@ -3,10 +3,10 @@ function AddObject(name2){
 	var loader = new THREE.OBJLoader();
 	loader.load(name2, function ( object ) {
 		//object.castShadow() = true;
-		//console.log(object.children.size);
+		console.log(object.children.size);
 		scene.add( object );},
 	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		//console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	},
 	function ( error ) {
 		alert( 'An error happened' );
