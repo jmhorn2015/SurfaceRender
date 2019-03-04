16cf2ed0382fd00bc5a8ca1a72d998466ecce67e
Jenna Horn <jmhorn2015@users.noreply.github.com>
2019-03-04 13:14:01 -0600

    flip normals

--- js/AddObject.js
+++ js/AddObject.js
@@ -1,9 +1,13 @@
 function AddObject(name2){
 	console.log("run");
 	var loader = new THREE.OBJLoader();
-	loader.load(name2, function ( object ) {
-		//object.castShadow() = true;
-		console.log(object.children.size);
+	loader.load(name2, function ( event ) {
+		var object = event.content;
+    	object.traverse( function ( child ) {
+        	if ( child instanceof THREE.Mesh ) {
+            	child.material.side = THREE.DoubleSide;
+        	}
+    	} );
 		scene.add( object );},
 	function ( xhr ) {
 		//console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
