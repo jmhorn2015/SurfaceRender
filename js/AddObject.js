0772953637a2b618ee09a43a65ff5e08fb2d44e6
Jenna Horn <jmhorn2015@users.noreply.github.com>
2019-03-04 15:55:47 -0600

    test

--- js/AddObject.js
+++ js/AddObject.js
@@ -2,11 +2,11 @@ function AddObject(name2){
 	console.log("run");
 	var loader = new THREE.OBJLoader();
 	loader.load(name2, function ( object ) {
-    	//object.traverse( function ( child ) {
-        	//if ( child instanceof THREE.Mesh ) {
-            	//child.material.side = THREE.DoubleSide;
-        	//}
-    	//} );
+    	object.traverse( function ( child ) {
+        	if ( child instanceof THREE.Mesh ) {
+            	child.material.side = THREE.DoubleSide;
+        	}
+    	} );
 		scene.add( object );},
 	function ( xhr ) {
 		//console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
