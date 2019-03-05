aa182e6264e21258e2c842373cb1d535acc42432
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-05 12:56:44 -0600

    test

--- js/Settings.js
+++ js/Settings.js
@@ -1,29 +1,14 @@
-function SettingThreeRadius(){
-	var counter = 0;
-	for(var i = 0; i < scene.children.length; i++){
-		var obj = scene.children[i];
-		if( obj.name == "mesh"){
-			circleShape = new THREE.Shape();
-			circleShape.moveTo( 0, circleRadius );
-			circleShape.quadraticCurveTo( circleRadius, circleRadius, circleRadius, 0 );
-			circleShape.quadraticCurveTo( circleRadius, - circleRadius, 0, - circleRadius );
-			circleShape.quadraticCurveTo( - circleRadius, - circleRadius, - circleRadius, 0 );
-			circleShape.quadraticCurveTo( - circleRadius, circleRadius, 0, circleRadius );
-			extrudeSettings.extrudePath = bigData[counter];
-			counter++;
-			obj.geometry = new THREE.ExtrudeBufferGeometry( circleShape, extrudeSettings );
-		}
-	}
-};
-function SettingThreeSteps(){
-	var counter = 0;
-	for(var i = 0; i < scene.children.length; i++){
-		var obj = scene.children[i];
-		if( obj.name == "mesh"){
-			extrudeSettings.steps = steps;
-			extrudeSettings.extrudePath = bigData[counter];
-			counter++;
-			obj.geometry = new THREE.ExtrudeBufferGeometry( circleShape, extrudeSettings );
-		}
+function Transparency(num){
+	var arr = scene.children;
+	for( int x = 0; x < arr.length; x++){
+		var object = arr[x];
+		object.traverse( function ( child ) {
+        	if ( child instanceof THREE.Mesh ) {
+            	//child.material.side = THREE.BackSide;
+            	//child.material.color.setHex(0x808080);
+				child.material.transparent = true;
+				child.material.opacity = num;
+        	}
+    	} 
 	}
 };
