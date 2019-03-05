900af24377abd62bb9ce705cf7e794104707e349
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-05 13:25:13 -0600

    test

--- js/Settings.js
+++ js/Settings.js
@@ -12,3 +12,14 @@ function Transparency(num){
     	});
 	}
 };
+function Hue(num){
+	var arr = scene.children;
+	for( var x = 0; x < arr.length; x++){
+		var object = arr[x];
+		object.traverse( function ( child ) {
+        	if ( child instanceof THREE.Mesh ) {
+            	child.material.color.setHSL(num, 1, .5);
+        	}
+    	});
+	}
+};
