c82df3feb9115b601c3c44d6b00c46bfcbc975ef
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-09 15:32:57 -0600

    test

--- js/AddObject.js
+++ js/AddObject.js
@@ -4,7 +4,7 @@ function AddObject(name2){
     	object.traverse( function ( child ) {
         	if ( child instanceof THREE.Mesh ) {
 				console.log("found mesh");
-            	child.material.side = THREE.BackSide;
+            	child.material.side = THREE.DoubleSide;
             	child.material.color.setHex(0x808080);
 				child.material.transparent = true;
 				child.material.opacity = .5;
