0fa5c90e33847cff1a00918d40dedf16a93862c5
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-12 14:26:04 -0500

    test

--- js/AddObject.js
+++ js/AddObject.js
@@ -1,4 +1,5 @@
 function AddObject(name2){
+	$("#loading").addClass('spinner-border');
 	var loader = new THREE.OBJLoader();
 	loader.load(name2, function ( object ) {
     	object.traverse( function ( child ) {
@@ -10,13 +11,14 @@ function AddObject(name2){
 				child.material.opacity = .5;
 				child.rotation.set(-90,0,0);
         	}
-			else if (child.isMaterial ){
+			else if (child.isMaterial()){
 				var temp = object;
 				console.log(name2);
 				//object = new THREE.Mesh( temp, new THREE.MeshPhongMaterial( { color: 0xffffff, dithering: true } ));
 			}
     	} );
-		scene.add( object );},
+		scene.add( object );
+		$("#loading").removeClass('spinner-border');},
 	function ( xhr ) {
 		
 	},
