f596a4684e8693182b05137361cc9c78cda90fc3
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-03-12 20:35:37 -0500

    test

--- js/Settings.js
+++ js/Settings.js
@@ -58,11 +58,14 @@ function Material(num){
 };
 function Texture(bool){
 	var arr = scene.children;
+	var textureMap = new THREE.TextureLoader().load( 'textures/UV_Grid_Sm.jpg' );
+	textureMap.wrapS = textureMap.wrapT = THREE.RepeatWrapping;
+	textureMap.anisotropy = 16;
 	for( var x = 0; x < arr.length; x++){
 		var object = arr[x];
 		object.traverse( function ( child ) {
         	if ( child instanceof THREE.Mesh & object.name == "surface") {
-            	
+            	texturedMaterial = new THREE.MeshPhongMaterial( { color: materialColor, map: textureMap, side: THREE.DoubleSide } );
         	}
     	});
 	}
@@ -93,11 +96,25 @@ function Shadow(bool){
 };
 function Reflection(bool){
 	var arr = scene.children;
+	var path = "data/skybox/";
+	var urls = [
+		path + "px.jpg", path + "nx.jpg",
+		path + "py.jpg", path + "ny.jpg",
+		path + "pz.jpg", path + "nz.jpg"
+	];
+	textureCube = new THREE.CubeTextureLoader().load( urls );
 	for( var x = 0; x < arr.length; x++){
 		var object = arr[x];
 		object.traverse( function ( child ) {
         	if ( child instanceof THREE.Mesh & object.name == "surface") {
-				
+				if(bool){
+					scene.background = textureCube;
+					child.material.envMap = textureCube;
+				}
+				else{
+					scene.background = null;
+					child.material.envMap = null;
+				}
         	}
     	});
 	}
