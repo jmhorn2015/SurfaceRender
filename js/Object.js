af3a9b763e884ce6aedfcbc7453e9797670e7fa3
Jenna Horn <43625335+jmhorn2015@users.noreply.github.com>
2019-04-09 14:06:15 -0500

    test

--- js/Object.js
+++ js/Object.js
@@ -214,7 +214,7 @@ class SRSurface extends SRMesh{
 		this.object.position.set(0, 0, -1);
 		this.object.castShadow = false;
 		this.object.receiveShadow = false;
-		scene.add(mesh);
+		scene.add(this.object);
 	};
 	
 }
